d00de73bb14f986e842f9b51b57fa69091fd6ecf
bubbagump2020 <bubbagum2020@gmail.com>
2019-10-21 11:18:41 -0500

    Models and Migrations

---
+++ db/migrate/20191021150825_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+      t.belongs_to :user
+      t.belongs_to :song
+
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
