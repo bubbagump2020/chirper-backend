24330fc82be59d983a16aabcc8df2a488bc58ad0
bubbagump2020 <bubbagum2020@gmail.com>
2019-10-21 12:15:51 -0500

    Adding user authentication via tokens

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,3 +1,10 @@
 class AuthenticationController < ApplicationController
-
+    def login
+        user, * = User.where({ email: params[:email] })
+        if(user && user.authenticate(params[:password]))
+            token = JWT.encode( { id: user.id }, 'asdljasldkfjs', 'HS256')
+            render json: { success: true, id: user.id, token: token }
+        else
+            render json: { success: false, id: nil }
+    end
 end
