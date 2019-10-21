d88ee03876a5eeef4d6308a3f3a5e433294c539a
bubbagump2020 <bubbagum2020@gmail.com>
2019-10-21 16:25:17 -0500

    Backend responding to fetch request from frontend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
+    before_action :define_current_user
+
     def create
         user = User.create(user_params)
         render json: user
     end
 
+    def show
+        render json: current_passenger
+    end
+
+    
+
     def user_params
         params.permit(:username, :email, :password)
+    end
+
+    def define_current_user
+        if params[:id]
+            @current_user = User.find(params[:id])
+        else
+            @current_user = User.new
+        end
+    end
+    
+    def current_user
+        @current_user
     end
 end
