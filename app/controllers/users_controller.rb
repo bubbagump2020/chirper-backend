1d2c4efb8c07b20a3336decd1612c600fb117c42
bubbagump2020 <bubbagum2020@gmail.com>
2019-10-21 11:58:13 -0500

    User validation, fixing some bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
 
+    def user_params
+        params.permit(:username, :email, :password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    validates :username, uniqueness: true
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6 }
     has_secure_password
-    has_many: reviews
+    has_many :reviews
+    # has_many :songs, through: :reviews
 end
